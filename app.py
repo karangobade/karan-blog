0ceeabedc35a711d914f2e55048f3a1cf05f136a
Karan Gobade <ssslandge12@gmail.com>
2026-06-30 12:27:08 +0530

    bb

--- app.py
+++ app.py
@@ -199,8 +199,8 @@ def logout():
     flash("Logged out.", "info")
     return redirect(url_for("home"))
 
- 
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
